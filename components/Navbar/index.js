24238442cc755df96848484e29dc4d45f0533d41
Mafessoni <andrea.mafessoni@mia-platform.eu>
2023-01-22 11:35:06 +0100

    Updated navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,14 +1,48 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { motion } from 'framer-motion'
+
 import styles from './Navbar.module.css'
 
-export default function Navbar () {
+const links = [
+  { name: 'About', href: '/about' },
+  { name: 'Work', href: '/work' },
+  { name: 'Contact', href: '/contact' }
+]
+
+const colorsByPathname = {
+  '/': 'var(--white)',
+  '/about': 'var(--blue)',
+  '/work': 'var(--white)',
+  '/contact': 'var(--white)'
+}
+
+function NavbarLinkItem ({ name, href, selected }) {
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">AM</Link>
+    <div className={styles.navbarLinkItem}>
+      <Link href={href}>{name}</Link>
+      <motion.div 
+        key={selected}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        style={{ opacity: selected ? 1 : 0 }}
+        className={styles.yellowDot} />
+    </div>
+  )
+} 
+
+export default function Navbar () {
+  const { pathname } = useRouter()
+
+  return (
+    <nav className={styles.navbar} style={{ color: colorsByPathname[pathname] }}>
+      <NavbarLinkItem name='AM' href='/' selected={pathname === '/'} />
       <ul>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/work">Work</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        {links.map(({ name, href }) => (
+          <li key={name}>
+            <NavbarLinkItem name={name} href={href} selected={pathname === href} />
+          </li>
+        ))}
       </ul>
     </nav>
   )
