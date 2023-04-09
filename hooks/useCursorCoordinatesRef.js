2f33cf3d666c805902f6227bf28d6eb5de3af7af
Mafessoni <andrea.mafessoni@mia-platform.eu>
2023-04-09 12:09:53 +0200

    Removed setTimeout in useCursorCoordinatesRef

--- hooks/useCursorCoordinatesRef.js
+++ hooks/useCursorCoordinatesRef.js
@@ -2,28 +2,26 @@ import { useEffect, useRef } from 'react'
 import { useThree } from '@react-three/fiber'
 import { Vector3 } from 'three'
 
-export default function useCursorCoordinatesRef({ delay = 0 } = {}) {
+export default function useCursorCoordinatesRef() {
   const cursorCoordinatesRef = useRef(new Vector3())
   const { camera } = useThree()
 
   useEffect(() => {
     const handlePointerMove = (event) => {
-      setTimeout(() => {
-        const vec = new Vector3(
-          ( event.clientX / window.innerWidth ) * 2 - 1,
-          - ( event.clientY / window.innerHeight ) * 2 + 1,
-          0.5
-        ) 
+      const vec = new Vector3(
+        ( event.clientX / window.innerWidth ) * 2 - 1,
+        - ( event.clientY / window.innerHeight ) * 2 + 1,
+        0.5
+      ) 
   
-        vec.unproject(camera)
-        vec.sub(camera.position).normalize()
+      vec.unproject(camera)
+      vec.sub(camera.position).normalize()
   
-        const distance = - camera.position.z / vec.z
-        const pos = new Vector3() 
-        pos.copy(camera.position).add(vec.multiplyScalar(distance))
+      const distance = - camera.position.z / vec.z
+      const pos = new Vector3() 
+      pos.copy(camera.position).add(vec.multiplyScalar(distance))
   
-        cursorCoordinatesRef.current = pos
-      }, delay)
+      cursorCoordinatesRef.current = pos
     }
   
     window.addEventListener('pointermove', handlePointerMove)
