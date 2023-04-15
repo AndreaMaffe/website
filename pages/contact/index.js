fb7e3cd213db854a8bcbcd4b7198280b8d12bc6b
Mafessoni <andrea.mafessoni@mia-platform.eu>
2023-04-15 11:56:47 +0200

    Edited Contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,4 +1,8 @@
 import { motion } from 'framer-motion'
+import { EnvelopeClosedIcon } from '@radix-ui/react-icons'
+
+import styles from './Contact.module.css'
+import Button from '../../components/Button'
 
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
@@ -6,21 +10,44 @@ const variants = {
   exit: { opacity: 0, x: 0, y: -100 },
 }
 
-export default function Skills() {
+export default function Contact() {
   return (
-    <motion.div
-      key='skills'
+    <motion.main
+      className={styles.contact}
+      key='contact'
       initial="hidden"
       animate="enter"
       exit="exit"
       variants={variants}
       transition={{ type: 'linear', duration: 1 }}
-      style={{ height: '100vh', display: 'flex', alignItems: 'center', padding: '100px' }}
     >
-      <div>
-        <h1>Skills</h1>
-        <p>This is a skills page</p>
-      </div>
-    </motion.div>
+      <h1>Let&#39;s build something <span style={{ color: 'var(--yellow)' }}>together</span>.</h1>
+
+      <form onSubmit={e => {e.preventDefault(); console.log(e)}}>
+        <input 
+          id='name'
+          name='name'
+          placeholder='Your name'
+          type='text'
+        />
+        <input 
+          id='email'
+          name='email'
+          placeholder='Your email'
+          type='email'
+        />
+        <textarea 
+          id='message' 
+          name='email'
+          placeholder='Your message'
+          rows={1}
+        />
+        <Button 
+          icon={<EnvelopeClosedIcon />}
+          type='submit'>
+            Send
+        </Button>
+      </form>
+    </motion.main>
   )
 }
