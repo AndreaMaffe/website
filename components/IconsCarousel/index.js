6cc8bf70e6874c8f35399cfbe3995aa4f9f86ae9
Mafessoni <andrea.mafessoni@mia-platform.eu>
2023-07-02 14:38:57 +0200

    Added icons to carousel

--- components/IconsCarousel/index.js
+++ components/IconsCarousel/index.js
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion'
 import { 
   SiAngular,
+  SiAmazonaws,
   SiCss3,
   SiDocker,
   SiHtml5,
@@ -9,8 +10,13 @@ import {
   SiNetlify,
   SiNextdotjs,
   SiNodedotjs,
+  SiPlaywright,
+  SiPostgresql,
   SiReact,
+  SiRedux,
   SiSpringboot,
+  SiStorybook,
+  SiStrapi,
   SiSvelte,
   SiTypescript,
   SiUnity,
@@ -20,27 +26,35 @@ import {
 import styles from './IconsCarousel.module.css'
 
 const ICONS = [
-  { icon: SiAngular, key: 'angular' },
-  { icon: SiCss3, key: 'css3' },
-  { icon: SiDocker, key: 'docker' },
-  { icon: SiHtml5, key: 'html5' },
-  { icon: SiJavascript, key: 'javascript' },
-  { icon: SiMongodb, key: 'mongodb' },
-  { icon: SiNetlify, key: 'netlify' },
-  { icon: SiNextdotjs, key: 'next.js' },
-  { icon: SiNodedotjs, key: 'node.js' },
-  { icon: SiReact, key: 'react' },
-  { icon: SiSpringboot, key: 'springboot' },
-  { icon: SiSvelte, key: 'svelte' },
-  { icon: SiTypescript, key: 'typescript' },
-  { icon: SiUnity, key: 'unity' },
-  { icon: SiVuedotjs, key: 'vue.js' }
+  { icon: SiAngular, key: 'Angular' },
+  { icon: SiAmazonaws, key: 'AWS' },
+  { icon: SiCss3, key: 'CSS3' },
+  { icon: SiDocker, key: 'Docker' },
+  { icon: SiHtml5, key: 'HTML5' },
+  { icon: SiJavascript, key: 'Javascript' },
+  { icon: SiMongodb, key: 'MongoDB' },
+  { icon: SiNetlify, key: 'Netlify' },
+  { icon: SiNextdotjs, key: 'Next.js' },
+  { icon: SiNodedotjs, key: 'Node.js' },
+  { icon: SiPlaywright, key: 'Playwright' },
+  { icon: SiPostgresql, key: 'PostgreSQL' },
+  { icon: SiReact, key: 'React' },
+  { icon: SiRedux, key: 'Redux' },
+  { icon: SiSpringboot, key: 'Springboot' },
+  { icon: SiStorybook, key: 'Storybook' },
+  { icon: SiStrapi, key: 'Strapi' },
+  { icon: SiSvelte, key: 'Svelte' },
+  { icon: SiTypescript, key: 'Yypescript' },
+  { icon: SiUnity, key: 'Unity' },
+  { icon: SiVuedotjs, key: 'Vue.js' }
 ]
+
+const ANIMATION_DURATION = 36
 
 export default function IconCarousel() {
   const iconsRow = (
     <motion.div 
-      animate={{ x: '-100%', transition: { duration: 24, ease: 'linear', repeat: Infinity } }}
+      animate={{ x: '-100%', transition: { duration: ANIMATION_DURATION, ease: 'linear', repeat: Infinity } }}
       style={{ display: 'flex', justifyContent: 'flex-start', gap: '24px' }}
     >
       {ICONS.map(({ icon: Icon, key }) => (
